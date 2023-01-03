8625dc6a8554dd556ce81247ce5ac159eda0c6a2
Restyled.io <commits@restyled.io>
2023-01-03 13:31:56 +0000

    Restyled by yapf

--- main.py
+++ main.py
@@ -32,8 +32,7 @@ def main():
     length_run = True
     time_run = True
 
-    print(
-        r"""
+    print(r"""
                                            ,////,
                                            /// 6|
                                            //  _|
@@ -50,10 +49,8 @@ def main():
                                              `\  \
                                                `\ \
                                                  \_\__
-                                                  \___)"""
-    )
-    print(
-        r"""
+                                                  \___)""")
+    print(r"""
                                         _
                                         (_)
                     _ __ _   _ _ __  _ __  _ _ __   __ _
@@ -62,54 +59,61 @@ def main():
                 |_|   \__,_|_| |_|_| |_|_|_| |_|\__, |
                                                  __/ |
                                                 |___/
-    """
-    )
+    """)
     print("              WELCOME TO THE RUNNING SPEED CALCULATOR")
     print("    Please answer each question to receive your time and speed!")
-    print("Note: Currently, the program only displays results in metric values. \n")
+    print(
+        "Note: Currently, the program only displays results in metric values. \n"
+    )
 
     # LENGTH ENTRY
     while run == True:
         # Type of Measurement
         while length_run == True:
             length_type = str(input("What unit will you be inputting? "))
-            if length_type.lower() == "marathons" or length_type.lower() == "marathon":
+            if length_type.lower() == "marathons" or length_type.lower(
+            ) == "marathon":
                 unit = "marathon"
                 print("You are now entering in marathons! \n")
                 length_run = False
-            elif (
-                length_type.lower() == "half marathons"
-                or length_type.lower() == "half marathon"
-            ):
+            elif (length_type.lower() == "half marathons"
+                  or length_type.lower() == "half marathon"):
                 unit = "halfmarathon"
                 print("You are now entering in half-marathons! \n")
                 length_run = False
-            elif length_type.lower() == "miles" or length_type.lower() == "mile":
+            elif length_type.lower() == "miles" or length_type.lower(
+            ) == "mile":
                 unit = "miles"
                 print("You are now entering in miles! \n")
                 length_run = False
-            elif length_type.lower() == "feet" or length_type.lower() == "foot":
+            elif length_type.lower() == "feet" or length_type.lower(
+            ) == "foot":
                 unit = "feet"
                 print("You are now entering in feet! \n")
                 length_run = False
-            elif (
-                length_type.lower() == "kilometers"
-                or length_type.lower() == "kilometer"
-            ):
+            elif (length_type.lower() == "kilometers"
+                  or length_type.lower() == "kilometer"):
                 unit = "kilometers"
                 print("You are now entering in kilometers! \n")
                 length_run = False
-            elif length_type.lower() == "meters" or length_type.lower() == "meter":
+            elif length_type.lower() == "meters" or length_type.lower(
+            ) == "meter":
                 unit = "meters"
                 print("You are now entering in meters! \n")
                 length_run = False
             elif length_type.lower() == "license":
-                print("Running Calculator Copyright (C) 2022-2023 @willtheorangeguy")
+                print(
+                    "Running Calculator Copyright (C) 2022-2023 @willtheorangeguy"
+                )
                 print(
                     "This program comes with ABSOLUTELY NO WARRANTY; for details view the license."
                 )
-                print("This is free software, and you are welcome to redistribute it")
-                print("under certain conditions; view the license for details. \n")
+                print(
+                    "This is free software, and you are welcome to redistribute it"
+                )
+                print(
+                    "under certain conditions; view the license for details. \n"
+                )
                 length_run = True
             elif length_type.lower() == "quit":
                 length_run = False
@@ -133,9 +137,12 @@ def main():
             try:  # enable only numerical values
                 time_run = False
                 length = float(input("How far did you run? "))
-                hours = int(input("How many hours did it take you? (if < 1, enter 0) "))
+                hours = int(
+                    input(
+                        "How many hours did it take you? (if < 1, enter 0) "))
                 mins = int(
-                    input("How many minutes did it take you? (if < 1, enter 0) ")
+                    input(
+                        "How many minutes did it take you? (if < 1, enter 0) ")
                 )
                 secs = float(input("How many seconds did it take you? "))
             except ValueError:
@@ -189,13 +196,9 @@ def main():
             print("You travelled " + str(round(distance, 2)) + " meters.")
         else:
             print("You travelled " + str(round(distance, 2)) + " kilometers.")
-        print(
-            "Your speed was "
-            + str(round(metersps, 2))
-            + " meters per second or "
-            + str(round(kmph, 2))
-            + " kilometers per hour. \n\n"
-        )
+        print("Your speed was " + str(round(metersps, 2)) +
+              " meters per second or " + str(round(kmph, 2)) +
+              " kilometers per hour. \n\n")
 
         length_run = True
         time_run = True
